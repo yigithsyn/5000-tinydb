b9033ace314beedf05829854d64c7df87a20c714
Hüseyin YİĞİT <yigit.hsyn@outlook.com>
2018-03-28 11:30:10 +0300

    Reduction for printing.

--- app.py
+++ app.py
@@ -1,59 +1,3 @@
-###############################################################################
-# Notifier
-###############################################################################
-from win32api import *
-from win32gui import *
-import win32con
-import sys, os
-import struct
-import time
- 
-class WindowsBalloonTip:
-    def __init__(self, title, msg):
-        message_map = {
-                win32con.WM_DESTROY: self.OnDestroy,
-        }
-        # Register the Window class.
-        wc = WNDCLASS()
-        hinst = wc.hInstance = GetModuleHandle(None)
-        wc.lpszClassName = "PythonTaskbar"
-        wc.lpfnWndProc = message_map # could also specify a wndproc.
-        classAtom = RegisterClass(wc)
-        # Create the Window.
-        style = win32con.WS_OVERLAPPED | win32con.WS_SYSMENU
-        self.hwnd = CreateWindow( classAtom, "Taskbar", style, \
-                0, 0, win32con.CW_USEDEFAULT, win32con.CW_USEDEFAULT, \
-                0, 0, hinst, None)
-        UpdateWindow(self.hwnd)
-        iconPathName = os.path.abspath(os.path.join( sys.path[0], "balloontip.ico" ))
-        icon_flags = win32con.LR_LOADFROMFILE | win32con.LR_DEFAULTSIZE
-        try:
-           hicon = LoadImage(hinst, iconPathName, \
-                    win32con.IMAGE_ICON, 0, 0, icon_flags)
-        except:
-          hicon = LoadIcon(0, win32con.IDI_APPLICATION)
-        flags = NIF_ICON | NIF_MESSAGE | NIF_TIP
-        nid = (self.hwnd, 0, flags, win32con.WM_USER+20, hicon, "tooltip")
-        Shell_NotifyIcon(NIM_ADD, nid)
-        Shell_NotifyIcon(NIM_MODIFY, \
-                         (self.hwnd, 0, NIF_INFO, win32con.WM_USER+20,\
-                          hicon, "Balloon  tooltip",msg,200,title))
-        # self.show_balloon(title, msg)
-        time.sleep(10)
-        DestroyWindow(self.hwnd)
-		#UnregisterClass(classAtom, hinst)
-    def OnDestroy(self, hwnd, msg, wparam, lparam):
-        nid = (self.hwnd, 0)
-        Shell_NotifyIcon(NIM_DELETE, nid)
-        PostQuitMessage(0) # Terminate the app.
-
-def balloon_tip(title, msg):
-    w=WindowsBalloonTip(title, msg)
-
-###############################################################################
-# Main Code
-###############################################################################
-
 # Built-in modules
 import os
 import traceback
@@ -65,8 +9,6 @@ from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
 from tinydb import TinyDB as tinydb
 
-
-
 # database
 tinydbDatabase = tinydb("db.json")
 
@@ -76,7 +18,6 @@ CORS(app)
 api = Api(app)
 parser = reqparse.RequestParser()
 
-
 # serve index.html
 @app.route('/<path:path>')
 def static_proxy(path):
@@ -85,7 +26,6 @@ def static_proxy(path):
 @app.route('/')
 def homepage():
   return send_from_directory("test", "index.html")
-
 
 class TinyDB(Resource):
   def get(self):
@@ -99,11 +39,9 @@ class TinyDB_Table(Resource):
       item["id"] = item.doc_id
       items.append(item)
     return items
-
   def post(self, table):
     table = tinydbDatabase.table(table)
     return table.insert(request.json)
-
   def delete(self, table):
     tinydbDatabase.purge_table(table)
     return {}
@@ -117,7 +55,6 @@ class TinyDB_Item(Resource):
       return item
     except Exception:
       return {"error": {"type": "api", "msg": "TinyDB_Item: GET\n" + str(traceback.format_exc())}}
-
   def post(self, table, doc_id):
     table = tinydbDatabase.table(table)
     try:
@@ -125,7 +62,6 @@ class TinyDB_Item(Resource):
       return {}
     except Exception:
       return {"error": {"type": "api", "msg": "TinyDB_Item: POST\n" + str(traceback.format_exc())}}
-
   def delete(self, table, doc_id):
     table = tinydbDatabase.table(table)
     try:
@@ -134,10 +70,7 @@ class TinyDB_Item(Resource):
     except Exception:
       return {"error": {"type": "api", "msg": "TinyDB_Item: DEL\n" + str(traceback.format_exc())}}
 
-
 api.add_resource(TinyDB, '/tinydb')
 api.add_resource(TinyDB_Table, '/tinydb/<table>')
 api.add_resource(TinyDB_Item, '/tinydb/<table>/<doc_id>')
-
-balloon_tip('Service: 5000-tinydb', 'Started successfully!')
 app.run(port=5000)
